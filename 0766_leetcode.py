16c4b74de28415292974dbd39db1f24e3a4a4481
caio <oliveiracaio1@yandex.com>
2022-05-11 13:58:27 -0300

    exercises 88, 285, 297, 346, 362, 398, 766, 1047, 1275 and 1578.

---
+++ 0766_leetcode.py
@@ -0,0 +1,59 @@
+"""766. Toeplitz Matrix
+
+link: https://leetcode.com/problems/toeplitz-matrix/
+
+problem: Given an m x n matrix, return true if the matrix is Toeplitz.
+Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the
+same elements.
+
+Example 1:
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+
+Example 2:
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation:
+The diagonal "[1, 2]" has different elements.
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 20
+0 <= matrix[i][j] <= 99
+
+Follow up:
+- What if the matrix is stored on disk, and the memory is limited such that you
+can only load at most one row of the matrix into the memory at once?
+- What if the matrix is so large that you can only load up a partial row into
+the memory at once?
+"""
+
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        def check_upper():
+            for col in range(len(matrix[0])):
+                nr, nc = 0, col
+                while nc < len(matrix[0]) - 1 and nr < len(matrix) - 1:
+                    if matrix[nr][nc] != matrix[nr + 1][nc + 1]:
+                        return False
+                    nc += 1; nr += 1
+            return True
+
+        def check_lower():
+            for row in range(1, len(matrix)):
+                nr, nc = row, 0
+                while nr < len(matrix) - 1 and nc < len(matrix[0]) - 1:
+                    if matrix[nr][nc] != matrix[nr + 1][nc + 1]:
+                        return False
+                    nc += 1; nr += 1
+            return True
+
+        return check_upper() and check_lower()
